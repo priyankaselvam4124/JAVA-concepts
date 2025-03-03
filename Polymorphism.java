6125912b0db9317621d4d08bdbf54e2e61c1d233
S Priyanka <priyankaselvam4124@gmail.com>
2025-03-03 12:36:34 +0530

    Added Polymorphism example with method overloading & overriding

---
+++ Polymorphism.java
@@ -0,0 +1,42 @@
+//5 Polymorphism compile time(overloading) and run time (overriding)
+// TASK: Create shape class with draw() having 2 overloaded versions
+//create circle n rectangle class inheriting shape and overrriding draw()
+class Shape{
+    //overloading draw()
+    void draw(){
+        System.out.println("Drawing a shape");
+    
+    }
+    //overloading draw() 
+    void draw(String color){
+        System.out.println("Drawing a shape with color : " + color);
+    }
+}
+//circle class overriding draw() 
+class Circle extends Shape{
+ @Override
+ void draw(){
+    System.out.println("Drawing a circle");
+ }
+}
+//square class overriding draw()
+class Square extends Shape{
+    @Override
+    void draw(){
+        System.out.println("Drawing a square");
+    }
+}
+public class Polymorphism {
+    public static void main(String[] args) {
+    //method overloading - compile time polymorphism
+    Shape s1 = new  Shape();
+    s1.draw();
+    s1.draw("Blue");
+    //method overriding - run time polymorphism
+    Shape myShape = new Circle();
+    myShape.draw();
+
+    myShape = new Square();
+    myShape.draw();
+}
+}
